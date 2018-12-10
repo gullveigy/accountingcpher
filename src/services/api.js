1e99b2fc5f6679c22b0fe8505c78c8992297d312
YING YAN <1804094745@qq.com>
2018-12-11 01:36:01 +0800

    deploy backend to heroku and modify api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default() => {
   return axios.create({
     // baseURL: 'http://localhost:3000/'
-    baseURL: 'http://localhost:3000/'
+    baseURL: 'https://cypher2.herokuapp.com/'
   })
 }
